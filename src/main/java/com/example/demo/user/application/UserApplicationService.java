91e48d37407ffc454fa7687771fe45ce8bd8908d
zawzaw329 <111064851+zawzaw329@users.noreply.github.com>
2026-06-03 03:04:42 +0900

    add messageProperties file

--- src/main/java/com/example/demo/user/application/UserApplicationService.java
+++ src/main/java/com/example/demo/user/application/UserApplicationService.java
@@ -3,14 +3,23 @@ package com.example.demo.user.application;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class UserApplicationService {
+	
+	private final MessageSource messageSource;
+	
 	public Map<String,Integer> getGenderMap() {
 		Map<String,Integer> genderMap = new LinkedHashMap<>();
-		genderMap.put("男性", 1);
-		genderMap.put("女性", 2);
+		String male = messageSource.getMessage("male",null, null);
+		String female = messageSource.getMessage("female",null, null);
+		genderMap.put(male,1);
+		genderMap.put(female, 2);
 		return genderMap;
 	}
 }
