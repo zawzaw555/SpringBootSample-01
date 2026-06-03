1e1bde34551546aa0b4d5bf2407863ded672351e
zawzaw329 <111064851+zawzaw329@users.noreply.github.com>
2026-06-03 12:51:06 +0900

    ModelAttribute for binding

---
+++ src/main/java/com/example/demo/user/form/SignupForm.java
@@ -0,0 +1,19 @@
+package com.example.demo.user.form;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class SignupForm {
+	private String userId;
+	private String password;
+	private String userName;
+	
+	@DateTimeFormat(pattern = "yyy/MM/dd")
+	private Date birthday;
+	private Integer age;
+	private Integer gender;
+}
