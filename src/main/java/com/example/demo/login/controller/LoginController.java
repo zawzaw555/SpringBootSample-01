0600b544688c94e648317f805c02955cb130c4ee
gihyo-san <gihyo-san@example.com>
2026-06-10 03:37:14 +0900

    add layout files

--- src/main/java/com/example/demo/login/controller/LoginController.java
+++ src/main/java/com/example/demo/login/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.demo.login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -9,5 +10,11 @@ public class LoginController {
 	@GetMapping("/login")
 	public String getLogin() {
 		return "login/login";
+	}
+	
+	/** ユーザー一覧画面んリダイレクト */
+	@PostMapping("/login")
+	public String postLogin() {
+		return "redirect:/user/list";
 	}
 }
